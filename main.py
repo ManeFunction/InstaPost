0c1f78f5c44218121b639c73f8bf6f2b88e193a1
Ilia Petrov-Komotskii <mfunc@outlook.com>
2023-05-01 17:19:54 +0300

    Added login cycle to keep trying if Instagram being picky and fixed errors if there are non-image files also in the folder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from instagrapi import Client  # A Python library for Instagram API
 from PIL import Image  # A Python library for working with images
 from dotenv import load_dotenv  # Parameters environment
 import os  # A module for interacting with the operating system
+import glob # A module for working with files
 import random  # A module for generating random numbers
 import time  # A module for working with time
 
@@ -13,20 +14,28 @@ login = os.environ.get("LOGIN")
 password = os.environ.get("PASS")
 hashtags = os.environ.get("HASHTAGS")
 images_dir = os.environ.get("IMAGES_PATH")
-repeat_time = int(os.environ.get("TIME"))
+repeat_time = int(os.environ.get("POST_DELAY"))
+login_time = int(os.environ.get("LOGIN_DELAY"))
 
 # Creating an instance of the Client class
 cl = Client()
 
-# Logging in to Instagram with your username and password
-print("Logging in...")
-cl.login(login, password)
-print("Logged in as ", login)
+# Logging in cycle (trying to log in until successful)
+while True:
+    try:
+        print("Logging in...")
+        cl.login(login, password)
+        print("Logged in as", login)
+        break
+    except:
+        print(f"Failed to log in. Trying again in {login_time} seconds...")
+        time.sleep(login_time)
 
 # Creating an infinite loop
 while True:
-    # Getting the list of files in the folder
-    files = os.listdir(images_dir)
+    # Getting the list of images in the folder
+    extensions = ['*.png', '*.jpg', '*.jpeg']
+    files = [f for ext in extensions for f in glob.glob(os.path.join(images_dir, ext))]
     print("Total files: ", len(files))
 
     # Choosing a random file from the list
@@ -60,5 +69,5 @@ while True:
         break
 
     # Waiting for 8 hours (in seconds)
-    print("Waiting...")
+    print(f"Waiting {repeat_time} seconds...")
     time.sleep(repeat_time)
